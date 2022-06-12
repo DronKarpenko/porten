4bea91a7feede931d9e582b0b5637f54863d33b9
DronKarpenko <dron.donbass@gmail.com>
2022-06-12 16:56:19 +0300

    brands-swiper

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+var swiper = new Swiper(".brandsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  allowSlidePrev: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 300px
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
     },
-  });
+    // when window width is >= 1110px
+    1110: {
+      slidesPerView: 4,
+      spaceBetween: 0
+    }
+  }
+});
